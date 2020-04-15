44799883ba292f38caf461731af6beaa7add344f
wangqimeng <wangqimeng@mininglamp.com>
2020-04-15 18:05:42 +0800

    feat(spring-custom-cache): add module spring-custom-cache

---
+++ spring-custom-cache/src/main/java/com/boommanpro/spring/custom/cache/SpringCustomCacheApplication.java
@@ -0,0 +1,17 @@
+package com.boommanpro.spring.custom.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Start
+ * @author wangqimeng
+ */
+@SpringBootApplication
+public class SpringCustomCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCustomCacheApplication.class, args);
+    }
+
+}
---
+++ spring-custom-cache/src/test/java/com/boommanpro/spring/custom/cache/SpringCustomCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boommanpro.spring.custom.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCustomCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
