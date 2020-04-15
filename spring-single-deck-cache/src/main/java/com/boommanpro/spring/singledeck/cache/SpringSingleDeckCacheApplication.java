fcb291d6290018b5fe1f835198339b940b763e5f
wangqimeng <wangqimeng@mininglamp.com>
2020-04-15 09:55:57 +0800

    build(spring-cache-practice): add project spring-single-deck-cache

---
+++ spring-single-deck-cache/src/main/java/com/boommanpro/spring/singledeck/cache/SpringSingleDeckCacheApplication.java
@@ -0,0 +1,16 @@
+package com.boommanpro.spring.singledeck.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangqieng
+ */
+@SpringBootApplication
+public class SpringSingleDeckCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSingleDeckCacheApplication.class, args);
+    }
+
+}
---
+++ spring-single-deck-cache/src/test/java/com/boommanpro/spring/singledeck/cache/SpringSingleDeckCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boommanpro.spring.singledeck.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSingleDeckCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
